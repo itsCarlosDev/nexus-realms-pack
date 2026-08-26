4abebcf10eaf8d6bbeef56cd7944c6feae5e4318
Carlos Morales Artés <135174201+itsCarlosDev@users.noreply.github.com>
2026-08-27 01:40:39 +0200

    Textures v1.1 Nexus

--- kubejs/startup_scripts/nexus_corruption_registry.js
+++ kubejs/startup_scripts/nexus_corruption_registry.js
@@ -1,5 +1,9 @@
 StartupEvents.registry('block', event => {
 
+    // =====================================================
+    // GRIETAS DECORATIVAS ACTUALES
+    // =====================================================
+
     const scars = [
         'nexus_scar_1',
         'nexus_scar_2',
@@ -10,10 +14,39 @@ StartupEvents.registry('block', event => {
     scars.forEach(id => {
         event.create(id)
             .displayName('Grieta del Nexo')
-            .soundType('stone')
             .model(`kubejs:block/${id}`)
             .renderType('translucent')
-            .hardness(0.5)
+            .soundType('amethyst')
+            .hardness(0.1)
+            .noCollision()
+            .notSolid()
+            .noDrops()
+            .noItem()
+            .fullBlock(false)
+            .opaque(false)
+            .lightLevel(0.20)
+    })
+
+
+    // =====================================================
+    // RED CONECTADA DE CORRUPCIÓN
+    // =====================================================
+
+    const paths = [
+        'nexus_path_end',
+        'nexus_path_straight',
+        'nexus_path_corner',
+        'nexus_path_t',
+        'nexus_path_cross'
+    ]
+
+    paths.forEach(id => {
+        event.create(id)
+            .displayName('Grieta del Nexo')
+            .model(`kubejs:block/${id}`)
+            .renderType('translucent')
+            .soundType('amethyst')
+            .hardness(0.1)
             .noCollision()
             .notSolid()
             .noDrops()
